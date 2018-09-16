f5cf47b98e1ed53240edbf6cdcac57a9eacfc76a
hektonian <hektonian@gmail.com>
2018-09-16 12:00:54 +0300

    Corrected filename InMemoryReadOnlyDataSource => InMemoryDataSource

--- Hektonian.DataSource.InMemory/InMemoryDataSource.cs
+++ Hektonian.DataSource.InMemory/InMemoryDataSource.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Hektonian.DataSource.InMemory.Interfaces;
 using Hektonian.DataSource.Interfaces;
 
 namespace Hektonian.DataSource.InMemory
 {
-    public class InMemoryReadOnlyDataSource : IAsyncDataSource, IAsyncMutableDataSource
+    public class InMemoryDataSource : IAsyncDataSource, IAsyncMutableDataSource
     {
-        private readonly InMemoryDataStore _store;
+        private readonly IInMemoryDataStore _store;
 
-        public InMemoryReadOnlyDataSource(InMemoryDataStore store = null)
+        public InMemoryDataSource(IInMemoryDataStore store = null)
         {
             _store = store;
         }
