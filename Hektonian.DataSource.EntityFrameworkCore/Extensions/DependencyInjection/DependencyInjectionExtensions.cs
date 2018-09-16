6cafe0a33430fa34ddd76ccbbaa59b344ca3c7fd
hektonian <hektonian@gmail.com>
2018-09-16 12:33:29 +0300

    Added dependency injection

---
+++ Hektonian.DataSource.EntityFrameworkCore/Extensions/DependencyInjection/DependencyInjectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hektonian.DataSource.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hektonian.DataSource.EntityFrameworkCore.Extensions.DependencyInjection
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddEntityFrameworkDataSource<TDbContext>(this IServiceCollection services)
+        where TDbContext: DbContext => services.AddScoped<IAsyncDataSource, EfDataSource<TDbContext>>();
+    }
+}
